9f89369c0bd2c7208f3c68524404d149cbd9f1a1
raul chagas <raul_chagas@hotmail.com>
2022-10-20 10:57:19 -0300

    changed routes: configured root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'lists#index'
   resources :lists, only: %i[index show create new edit update] do
     resources :bookmarks, only: %i[create new]
   end
